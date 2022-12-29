4e2511caa461cc0698191819a6ddabbf57e68fbf
ldholl <lacy.d.holleman@gmail.com>
2022-12-29 15:00:21 -0500

    password validation added to user model and routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -47,8 +47,35 @@ router.post('/', (req, res) => {
     });
 });
 
+//login route
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData){
+            res.status(400).json({ message: 'No user found with this email'});
+            return;
+        }
+       // res.json({ user: dbUserData })
+
+       //verify user
+       const validPassword = dbUserData.checkPassword(req.body.password);
+       if(!validPassword){
+        res.status(400).json({ message: 'Incorrect password'});
+        return;
+       }
+       res.json({ user: dbUserData, message: 'You are now logged in!'});
+    });
+});
+
+
+
 router.put('/:id', (req, res) => {
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
