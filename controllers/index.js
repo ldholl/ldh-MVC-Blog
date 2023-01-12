d1c9a56a2b7c06595420eca36dbbd1ced10106a0
ldholl <lacy.d.holleman@gmail.com>
2023-01-12 09:32:40 -0500

    added something

--- controllers/index.js
+++ controllers/index.js
@@ -4,9 +4,9 @@ const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes')
 
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes)
+router.use('ldh-mvc-blog/api', apiRoutes);
+router.use('ldh-mvc-blog/', homeRoutes);
+router.use('ldh-mvc-blog/dashboard', dashboardRoutes)
 
 router.use((req, res) => {
     res.status(404).end();
